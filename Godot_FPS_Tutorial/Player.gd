bb2979e0347e101c48c866dcf3187c6014aa9661
Taner Esmeroğlu <taner.esmrglu@gmail.com>
2019-10-02 14:47:04 +0300

    half of the part 6 done

--- Godot_FPS_Tutorial/Player.gd
+++ Godot_FPS_Tutorial/Player.gd
@@ -219,8 +219,6 @@ func process_input(delta):
 	if Input.is_action_just_pressed("ui_cancel"):
 		if Input.get_mouse_mode() == Input.MOUSE_MODE_VISIBLE:
 			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-		else:
-			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	# -----------------------------
 	
 	# -----------------------------
