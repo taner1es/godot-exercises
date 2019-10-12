f3d3dcfd1229e076c1919a2aaed132db2f4cd60e
Taner Esmeroğlu <taner.esmrglu@gmail.com>
2019-10-12 15:28:14 +0300

    water in 2d

---
+++ Shading/First Shaders/GDQuest Tutorials/water/water.gd
@@ -0,0 +1,8 @@
+tool
+extends Sprite
+
+func calculate_aspect_ratio():
+	material.set_shader_param("aspect_ratio", scale.y / scale.x)
+
+func _on_Water_item_rect_changed():
+	calculate_aspect_ratio()
