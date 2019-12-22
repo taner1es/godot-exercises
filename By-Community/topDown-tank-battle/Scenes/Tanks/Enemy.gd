3cef19e5c88491a2704cfdc781232e1e46e51a7d
taner1es <taner.esmrglu@gmail.com>
2019-12-22 15:36:45 +0300

    topDown Tank Battle - Part 5 - enemy tank shooting - improvement of the enemy tank movement

--- By-Community/topDown-tank-battle/Scenes/Tanks/Enemy.gd
+++ By-Community/topDown-tank-battle/Scenes/Tanks/Enemy.gd
@@ -5,12 +5,19 @@ onready var parent = get_parent()
 export (float) var turret_speed
 export (int) var detect_radius
 
+var speed = 0
 var target = null
 
 func _ready():
+	$DetectRadius/CollisionShape2D.shape = CircleShape2D.new()
 	$DetectRadius/CollisionShape2D.shape.radius = detect_radius
 
 func control(delta):
+	if $LookAhead1.is_colliding() or $LookAhead2.is_colliding():
+		speed = lerp(speed, 0, 0.1)
+	else:
+		speed = lerp(speed, max_speed, 0.05)
+	
 	if parent is PathFollow2D:
 		parent.offset += speed * delta;
 		position = Vector2()
@@ -20,6 +27,8 @@ func _process(delta):
 		var target_dir = (target.global_position - global_position).normalized()
 		var current_dir = Vector2(1, 0).rotated($Turret.global_rotation)
 		$Turret.global_rotation = current_dir.linear_interpolate(target_dir, turret_speed * delta).angle()
+		if current_dir.dot(target_dir) > 0.9:
+			shoot()
 
 func _on_DetectRadius_body_entered(body):
 	target = body
