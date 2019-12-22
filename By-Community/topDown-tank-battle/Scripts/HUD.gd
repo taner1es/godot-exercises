ee1e3ccb75bfc046e0189a68f8e2808a6ac09649
taner1es <taner.esmrglu@gmail.com>
2019-12-22 16:37:55 +0300

    topDown Tank Battle - Part 6 - tanks take damage - HUD healthbar - HUD healtbar, damage animation

---
+++ By-Community/topDown-tank-battle/Scripts/HUD.gd
@@ -0,0 +1,21 @@
+extends CanvasLayer
+
+var bar_red = preload("res://Assets/Textures/barHorizontal_red_mid 200.png")
+var bar_green = preload("res://Assets/Textures/barHorizontal_green_mid 200.png")
+var bar_yellow = preload("res://Assets/Textures/barHorizontal_yellow_mid 200.png")
+var bar_texture
+
+func update_healtbar(value):
+	bar_texture = bar_green
+	if value < 60:
+		bar_texture = bar_yellow
+	if value < 25:
+		bar_texture = bar_red
+	
+	$MarginContainer/HBoxContainer/HealthBar.texture_progress = bar_texture
+	$MarginContainer/HBoxContainer/HealthBar/Tween.interpolate_property($MarginContainer/HBoxContainer/HealthBar, 
+			"value", $MarginContainer/HBoxContainer/HealthBar.value, value,
+			.2, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	$MarginContainer/HBoxContainer/HealthBar/Tween.start()
+	$MarginContainer/HBoxContainer/HealthBar/AnimationPlayer.play("Healthbar_flash")
+	#$MarginContainer/HBoxContainer/HealthBar.value = value
