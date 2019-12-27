57fcaef6a94105a5e0270f31431510b5ec974ba4
taner1es <taner.esmrglu@gmail.com>
2019-12-27 07:25:11 +0300

    procgen tut. - part 2 - concept of random seed value - erase some of the walls, because there is only one route from a point to another point according to algorithm that we used to create the maze. - generating gaps between the paths to open some place to be able to locate some environmental objects or decors - locate connector cells (indexed by 5(horziontal-connector) and 10(vertical-connector)) reasonably - distributing connector cells mid region - testing with isometric tileset

--- By-Community/Procedural Content Generation/Scripts/Maze.gd
+++ By-Community/Procedural Content Generation/Scripts/Maze.gd
@@ -6,22 +6,33 @@ const S = 4
 const W = 8
 
 var cell_walls = {
-					Vector2(0, -1): N, 
-					Vector2(1, 0):  E, 
-					Vector2(0, 1):  S, 
-					Vector2(-1, 0): W, 
+					Vector2(0, -2): N, 
+					Vector2(2, 0):  E, 
+					Vector2(0, 2):  S, 
+					Vector2(-2, 0): W, 
 				}
 
 var tile_size = 64 # tile size (in pixel)
-var width = 16 # width of map (in tile)
-var height = 9 # height of map (in tile)
+var width = 35 # width of map (in tile)
+var height = 20 # height of map (in tile)
+
+var map_seed = 0
 
 onready var Map = $TileMap
 
+var erase_fraction = .2
+
 func _ready():
+	$Camera2D.zoom = Vector2(3, 3)
+	$Camera2D.position = Map.map_to_world(Vector2(width/2, height/2))
 	randomize()
+	if !map_seed:
+		map_seed = randi()
+	seed(map_seed)
+	print("Seed: ", map_seed)
 	tile_size = Map.cell_size
 	make_maze()
+	erase_walls()
 	
 func check_neighbors(cell, unvisited): # return an array of unvisited neighbors
 	var list = []
@@ -38,8 +49,12 @@ func make_maze():
 	Map.clear()
 	for x in range(width):
 		for y in range(height):
-			unvisited.append(Vector2(x, y))
 			Map.set_cellv(Vector2(x, y), N|E|S|W)
+	
+	for x in range(0, width, 2):
+		for y in range(0, height, 2):
+			unvisited.append(Vector2(x, y))
+		
 	var current = Vector2(0, 0)
 	unvisited.erase(current)
 	# execute recursive backtracker algorithm
@@ -54,8 +69,37 @@ func make_maze():
 			var next_walls = Map.get_cellv(next) - cell_walls[-dir]
 			Map.set_cellv(current, current_walls)
 			Map.set_cellv(next, next_walls)
+			
+			# insert indermediate(connector) cell
+			if dir.x != 0:
+				Map.set_cellv(current + dir/2, 5)
+			else:
+				Map.set_cellv(current + dir/2, 10)
+			
+			
 			current = next
 			unvisited.erase(current)
 		elif stack:
 			current = stack.pop_back()
+		#yield(get_tree(), 'idle_frame')
+		
+func erase_walls():
+	# randomly remove a number of the map's walls
+	for i in range(int(width * height * erase_fraction)):
+		var x = int(rand_range(2, width/2 - 2)) * 2
+		var y = int(rand_range(2, height/2 - 2)) * 2
+		var cell = Vector2(x, y)
+		# pick random neighbor
+		var neighbor = cell_walls.keys()[randi() % cell_walls.size()]
+		# if there's a wall between them, remove it
+		if Map.get_cellv(cell) & cell_walls[neighbor]:
+			var walls = Map.get_cellv(cell) - cell_walls[neighbor]
+			var n_walls = Map.get_cellv(cell+neighbor) - cell_walls[-neighbor]
+			Map.set_cellv(cell, walls)
+			Map.set_cellv(cell+neighbor, n_walls)
+			# insert intermediate cell
+			if neighbor.x != 0:
+				Map.set_cellv(cell+neighbor/2, 5)
+			else:
+				Map.set_cellv(cell+neighbor/2, 10)
 		yield(get_tree(), 'idle_frame')
