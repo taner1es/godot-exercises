7b8d28132aca1d9b3b63aae23a43cb3c4fab9411
taner1es <taner.esmrglu@gmail.com>
2020-01-01 21:06:31 +0300

    procgen tut. - part 7 - find paths that connect rooms - generate and draw mst(minimum spanning tree) by using Prim's algorithm to mark up the hallways

--- By-Community/Procedural Content Generation/Dungeon Generation/Scripts/Main.gd
+++ By-Community/Procedural Content Generation/Dungeon Generation/Scripts/Main.gd
@@ -2,12 +2,14 @@ extends Node2D
 
 var Room = preload("res://Scenes/Room.tscn")
 
-var tile_size = 32
+var tile_size = 32 
 var num_rooms = 50
 var min_size = 4
 var max_size = 10
 var hspread = 400 # horizontal spread
 var cull = 0.5
+
+var path: AStar # Astar(A*) pathfinding object
 
 func _ready():
 	randomize()
@@ -24,16 +26,29 @@ func make_rooms():
 	# wait for movement to stop
 	yield(get_tree().create_timer(1.1), "timeout")
 	# cull rooms
+	var room_positions = []
 	for room in $Rooms.get_children():
 		if randf() < cull:
 			room.queue_free()
 		else:
 			room.mode = RigidBody2D.MODE_STATIC
-		
-		
+			room_positions.append(Vector3(room.position.x, room.position.y, 0))
+	
+	yield(get_tree(), "idle_frame")
+	# generate a minimum spanning tree connecting the rooms
+	path = find_mst(room_positions)
+	
 func _draw():
 	for room in $Rooms.get_children():
-		draw_rect(Rect2(room.position - room.size, room.size*2), Color(32,228,0), false)
+		draw_rect(Rect2(room.position - room.size, room.size*2), Color("#6eff00"), false)
+		
+	if path:
+		for p in path.get_points(): # each position
+			for c in path.get_point_connections(p): # each connection
+				var pp = path.get_point_position(p)
+				var cp = path.get_point_position(c)
+				draw_line(Vector2(pp.x, pp.y), Vector2(cp.x, cp.y), Color(1, 15, 0), 15, true)
+			
 		
 func _process(delta):
 	update()
@@ -42,10 +57,36 @@ func _input(event):
 	if event.is_action_pressed("ui_accept"):
 		for room in $Rooms.get_children():
 			room.queue_free()
+		path = null
 		make_rooms()
 		
 		
+	
+func find_mst(nodes: Array):
+	# Prim's Algorithm
+	var path = AStar.new()
+	path.add_point(path.get_available_point_id(), nodes.pop_front())
 		
+	# Repeat until no more nodes remain
+	while nodes:
+		var min_dist = INF 	# minimum distance
+		var min_p = null 	# minimum position
+		var p = null 		# current position
 		
+		# Loop through points the path
+		for p1 in path.get_points():
+			p1 = path.get_point_position(p1)
+			# Loop through remaining nodes
+			for p2 in nodes:
+				if p1.distance_to(p2) < min_dist:
+					min_dist = p1.distance_to(p2)
+					min_p = p2
+					p = p1
 		
+		var n = path.get_available_point_id()
+		path.add_point(n, min_p)
+		path.connect_points(path.get_closest_point(p), n)
+		nodes.erase(min_p)
+		
+	return path
 		
